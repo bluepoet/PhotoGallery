ec678e007ec9091945a34b830a86269fc71d111f
bluepoet <bluepoet1112@gmail.com>
2017-02-22 08:52:49 +0900

    finish chapter24 exercise

--- app/src/main/java/net/bluepoet/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/net/bluepoet/photogallery/PhotoGalleryFragment.java
@@ -1,8 +1,11 @@
 package net.bluepoet.photogallery;
 
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
@@ -38,7 +41,17 @@ public class PhotoGalleryFragment extends Fragment {
         setRetainInstance(true);
         new FetchItemsTask().execute();
 
-        mThumbnailDownloader = new ThumbnailDownloader<>();
+        Handler responseHandler = new Handler();
+        mThumbnailDownloader = new ThumbnailDownloader<>(responseHandler);
+        mThumbnailDownloader.setThumbnailDownloadListener(
+                new ThumbnailDownloader.ThumbnailDownloadListener<PhotoHolder>() {
+                    @Override
+                    public void onThumbnailDownloaded(PhotoHolder target, Bitmap thumbnail) {
+                        Drawable drawable = new BitmapDrawable(getResources(), thumbnail);
+                        target.bindDrawable(drawable);
+                    }
+                }
+        );
         mThumbnailDownloader.start();
         mThumbnailDownloader.getLooper();
         Log.i(TAG, "background thread start!");
@@ -100,7 +113,7 @@ public class PhotoGalleryFragment extends Fragment {
             GalleryItem galleryItem = mGalleryItems.get(position);
             Drawable drawable = getResources().getDrawable(R.drawable.bill_up_close);
             photoHolder.bindDrawable(drawable);
-            mThumbnailDownloader.ff(photoHolder, galleryItem.getUrl());
+            mThumbnailDownloader.queueThumbnail(photoHolder, galleryItem.getUrl());
         }
 
         @Override
@@ -121,5 +134,11 @@ public class PhotoGalleryFragment extends Fragment {
             mItems = galleryItems;
             setupAdapter();
         }
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        mThumbnailDownloader.cleanQueue();
     }
 }
